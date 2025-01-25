9a2878281278728d940435a524b177a52fd72e49
Navnit rai <99525523+Navnit73@users.noreply.github.com>
2025-01-25 21:55:56 +0530

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -3,11 +3,11 @@
 export const data = [
   {
     id: 1,
-    img: 'https://i.postimg.cc/VkRWdHWx/57214s81.jpg',
-    title: "Multimedia Web",
+    img: 'https://res.cloudinary.com/ddxu2wqfm/image/upload/v1737822334/React_App_-_Google_Chrome_1_25_2025_9_53_53_PM_myolzo.png',
+    title: "Learnx subscription based learning ",
     desc: "",
-    link: "",
-    git: "",
+    link: "https://learnx-frontend.vercel.app/",
+    git: "https://github.com/Navnit73/Learnx-frontend",
   },
   {
     id: 2,
